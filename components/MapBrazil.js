72a3379c247a8caf292f381381491cf920e45a69
Juliano Farias <julianorsfarias@msn.com>
2020-04-08 01:39:58 +0200

    ✨add back button and legends to map

--- components/MapBrazil.js
+++ components/MapBrazil.js
@@ -1,12 +1,19 @@
 import { useState, useEffect } from "react"
-import { Mercator } from "@vx/geo"
 import { ParentSize } from "@vx/responsive"
-import { scaleLinear } from "@vx/scale"
+import { scaleLinear, scaleThreshold } from "@vx/scale"
+import { Mercator } from "@vx/geo"
+import {
+	LegendLinear,
+	LegendThreshold,
+	LegendItem,
+	LegendLabel,
+} from "@vx/legend"
 import * as topojson from "topojson"
-import { Box } from "@chakra-ui/core"
+import { Text, Flex, Box, Link as ChakraLink } from "@chakra-ui/core"
 import styled from "@emotion/styled"
 import { css } from "@emotion/core"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 import topology from "../utils/simpleTopology.json"
 
@@ -25,8 +32,7 @@ const State = styled.path`
 	${selectedStyles}
 `
 
-const Cities = ({ cities, projection }) => {
-	const radiusScale = scaleLinear({ domain: [1, 5000], range: [3, 20] })
+const Cities = ({ cities, projection, scale }) => {
 	return (
 		<g>
 			{cities.map((city) => {
@@ -36,7 +42,7 @@ const Cities = ({ cities, projection }) => {
 						key={city.city_ibge_code}
 						cx={position[0]}
 						cy={position[1]}
-						r={radiusScale(city.confirmed)}
+						r={scale(city.confirmed)}
 						fill="red"
 						opacity={0.35}
 					/>
@@ -46,24 +52,32 @@ const Cities = ({ cities, projection }) => {
 	)
 }
 
-const MapBrazil = ({
-	results,
-	selectedState = null,
-	onStateSelection,
-	cities,
-}) => {
+const MapBrazil = ({ results, cities }) => {
+	const router = useRouter()
+	const { state } = router.query
 	const world = topojson.feature(topology, topology.objects.states)
 	const outline = topojson.mesh(topology, topology.objects.states)
+	const radiusScale = scaleThreshold({
+		domain: [1, 10, 100, 1000, 10000],
+		range: [2, 4, 6, 8, 10],
+	})
 	const colorScale = scaleLinear({
 		domain: [0, Math.max(...results.map((r) => r.confirmed))],
 		range: ["white", "red"],
 	})
 	const [offset, setOffset] = useState(0)
 	useEffect(() => {
-		selectedState ? setOffset(25) : setOffset(0)
-	}, [selectedState])
+		state ? setOffset(25) : setOffset(0)
+	}, [state])
 	return (
-		<Box height={[350, 475, 600]} mt={8}>
+		<Box height={[350, 475, 600]} mt={8} position="relative">
+			{state && (
+				<Box position="absolute" t={0} l={0}>
+					<Link href="/" replace>
+						<ChakraLink>Voltar</ChakraLink>
+					</Link>
+				</Box>
+			)}
 			<ParentSize>
 				{({ width: w, height: h }) => (
 					<svg width={w} height={h}>
@@ -74,9 +88,7 @@ const MapBrazil = ({
 									[offset, offset],
 									[w - offset, h - offset],
 								],
-								selectedState
-									? world.features.find((f) => f.id === selectedState)
-									: outline,
+								state ? world.features.find((f) => f.id === state) : outline,
 							]}
 						>
 							{(mercator) => {
@@ -86,7 +98,7 @@ const MapBrazil = ({
 											<Link key={f.id} href="/[state]" as={`/${f.id}`}>
 												<State
 													fill={
-														selectedState
+														state
 															? "white"
 															: colorScale(
 																	results.find((r) => r.state === f.id)
@@ -94,24 +106,21 @@ const MapBrazil = ({
 															  )
 													}
 													d={mercator.path(f)}
-													onClick={() => onStateSelection(f.id)}
 												/>
 											</Link>
 										))}
-										{selectedState && (
+										{state && (
 											<g>
 												<Link href="/" replace>
 													<State
 														fill={colorScale(
-															results.find((r) => r.state === selectedState)
-																.confirmed
+															results.find((r) => r.state === state).confirmed
 														)}
 														d={mercator.path(
 															mercator.features.find(
-																(f) => f.feature.id === selectedState
+																(f) => f.feature.id === state
 															).feature
 														)}
-														onClick={() => onStateSelection(null)}
 														variant="selected"
 													/>
 												</Link>
@@ -120,9 +129,10 @@ const MapBrazil = ({
 														cities={cities}
 														projection={
 															mercator.features.find(
-																(f) => f.feature.id === selectedState
+																(f) => f.feature.id === state
 															).projection
 														}
+														scale={radiusScale}
 													/>
 												)}
 											</g>
@@ -134,6 +144,67 @@ const MapBrazil = ({
 					</svg>
 				)}
 			</ParentSize>
+			<Flex
+				direction="column"
+				position="absolute"
+				bottom={0}
+				left={0}
+				p={2}
+				border="1px"
+				bg="whiteAlpha.500"
+			>
+				{state ? (
+					<LegendThreshold scale={radiusScale}>
+						{(labels) => {
+							return labels.slice(1).map((label, i) => (
+								<LegendItem key={`legend-threshold-${i}`}>
+									<Flex mr={2}>
+										<svg width={label.value * 2} height={label.value * 2}>
+											<circle
+												cx={label.value}
+												cy={label.value}
+												r={label.value}
+												fill="red"
+												opacity={0.35}
+											/>
+										</svg>
+									</Flex>
+									<LegendLabel>
+										<Text m={0} fontSize="xs">
+											{label.text}
+										</Text>
+									</LegendLabel>
+								</LegendItem>
+							))
+						}}
+					</LegendThreshold>
+				) : (
+					<LegendLinear scale={colorScale}>
+						{(labels) => {
+							return labels.map((label, i) => (
+								<LegendItem key={`legend-threshold-${i}`}>
+									<Flex mr={2}>
+										<svg width={15} height={15}>
+											<rect
+												width={15}
+												height={15}
+												fill={label.value}
+												stroke="black"
+												strokeWidth={0.5}
+											/>
+										</svg>
+									</Flex>
+									<LegendLabel>
+										<Text m={0} fontSize="xs">
+											{label.text}
+										</Text>
+									</LegendLabel>
+								</LegendItem>
+							))
+						}}
+					</LegendLinear>
+				)}
+			</Flex>
 		</Box>
 	)
 }
