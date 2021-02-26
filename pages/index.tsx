3edc814feb7115fa7e005e8f76657e10a5c360eb
risaddex <risaddex@gmail.com>
2021-02-25 21:52:33 -0300

    Reordered chart widgets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
-import { Stack, Flex, Text, Box } from '@chakra-ui/react'
-import compareAsc from 'date-fns/compareAsc'
+import { Stack, Flex, Text, Box, Spacer } from '@chakra-ui/react'
 
 import * as Country from '../components/Country'
 import Footer from '../components/Footer'
 import PageHeader from '../components/PageHeader'
 import LastUpdateInfo from '../components/LastUpdateInfo'
 import BarChartByTime from '../components/BarChartByTime'
-import counties from '../utils/counties.json'
-import fetchAllReports, { requestOptions } from '../utils/fetchAllReports'
 
+import counties from '../utils/counties.json'
 import topology from '../public/topologies/brazil.json'
+
+import fetchAllReports, { requestOptions } from '../utils/fetchAllReports'
 import { CountrySumReport, Report } from '../utils/types'
-import { totalSumByDay } from '../utils/filters';
-import { GetStaticProps } from 'next';
+import { totalSumByDay } from '../utils/filters'
 
 interface HomeProps {
   lastReports: Report[]
-  previousReports: Report[]
+  // previousReports: Report[]
   lastUpdate: string
   reportsByCounty: Report[]
   countrySumByDay: CountrySumReport[]
@@ -24,7 +23,7 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = ({
   lastReports,
-  previousReports,
+  // previousReports,
   lastUpdate,
   reportsByCounty,
   countrySumByDay,
@@ -47,41 +46,41 @@ const Home: React.FC<HomeProps> = ({
           <Country.Map results={lastReports} topology={topology} />
         </Box>
         <Box w={['100%', '100%', '50%']}>
-          <Country.TotalResults data={countrySumByDay} />
+          <Box mb="5">
+            <BarChartByTime
+              data={countrySumByDay.map(({ date, newConfirmed }) => ({
+                date,
+                value: newConfirmed,
+              }))}
+              color="red"
+              title="Casos novos por dia"
+            />
+          </Box>
+          <Box>
+            <BarChartByTime
+              data={countrySumByDay.map(({ date, newDeaths }) => ({
+                date,
+                value: newDeaths,
+              }))}
+              color="gray"
+              title="Óbitos por dia"
+            />
+          </Box>
         </Box>
       </Flex>
       <LastUpdateInfo lastUpdate={lastUpdate} />
       <Box>
-        <BarChartByTime
-          data={countrySumByDay.map(({ date, newConfirmed }) => ({
-            date,
-            value: newConfirmed,
-          }))}
-          color="red"
-          title="Casos novos por dia"
-        />
-      </Box>
-      <Box>
-        <BarChartByTime
-          data={countrySumByDay.map(({ date, newDeaths }) => ({
-            date,
-            value: newDeaths,
-          }))}
-          color="gray"
-          title="Óbitos por dia"
-        />
-      </Box>
-      <Box>
+        <Country.TotalResults data={countrySumByDay} />
         <Country.NormalizedConfirmed reportsByCounty={reportsByCounty} />
       </Box>
-      <Stack spacing={2}>
+      <Stack spacing="5" direction="column">
         <Text fontSize="lg" color="gray.500">
           Dados por estado:
         </Text>
-        <Country.DataTable
+        {/* <Country.DataTable
           previousReports={previousReports}
           lastReports={lastReports}
-        />
+        /> */}
       </Stack>
       <Footer />
     </Stack>
@@ -89,9 +88,9 @@ const Home: React.FC<HomeProps> = ({
 )
 
 export async function getStaticProps() {
-  // ?Busca todos os reports
+  // ? Busca todos os reports
   const reports = await fetchAllReports(`place_type=state`)
-  // ?Atualiza os recentes
+  // ? Atualiza os recentes
   const lastReports = reports.filter(({ is_last: isLast }) => isLast)
   // ? Soma total diária do PAÍS
   const countrySumByDay = totalSumByDay(reports)
@@ -105,9 +104,9 @@ export async function getStaticProps() {
     return acc
   }, {})
   // ? boletins Anteriores
-  const previousReports = counties.map(({ initials }) =>
-    reports.find((report) => report.state === initials && !report.is_last)
-  )
+  // const previousReports = counties.map(({ initials }) =>
+  //   reports.find((report) => report.state === initials && !report.is_last)
+  // )
   const { tables } = await fetch(
     `https://api.brasil.io/v1/dataset/covid19/`,
     requestOptions
@@ -118,7 +117,7 @@ export async function getStaticProps() {
       countrySumByDay,
       reportsByCounty,
       lastReports,
-      previousReports,
+      // previousReports,
       lastUpdate: tables[1].import_date,
     },
   }
